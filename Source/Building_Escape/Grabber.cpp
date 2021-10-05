47067db4e2cc7b4e1e4e9ba1d9eb831d5683b0e6
bluenight12 <ekehd258@gmail.com>
2021-10-05 17:00:38 +0900

    collision & LineTrace

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,66 @@
+// Copyright KDG 2021
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
+
+    // ...
+}
+
+// Called when the game starts
+void UGrabber::BeginPlay() {
+    Super::BeginPlay();
+
+    UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation);
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	AActor* ActorHit = Hit.GetActor();
+	if(ActorHit){
+		UE_LOG(LogTemp, Error, TEXT("Line trace has hit : %s"), *(ActorHit->GetName()));
+	}
+
+    // Ray-cast out to a certain distance
+}
--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -1,8 +1,9 @@
 // Copyright KDG 2021
 
 #include "OpenDoor.h"
-
+#include "Engine/World.h"
 #include "GameFrameWork/Actor.h"
+#include "GameFramework/PlayerController.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor() {
@@ -19,20 +20,38 @@ void UOpenDoor::BeginPlay() {
     Super::BeginPlay();
     InitialYaw = GetOwner()->GetActorRotation().Yaw;
     CurrentYaw = InitialYaw;
-    TargetYaw += InitialYaw;
+    OpenAngle += InitialYaw;
+
+    if (!PressurePlate) {
+        UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplate set!"), *GetOwner()->GetName());
+    }
+
+    ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-    if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+    if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens)) {
         OpenDoor(DeltaTime);
+        DoorLastOpened = GetWorld()->GetTimeSeconds();
+
+    } else {
+        if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorCloseDelay) {
+            CloseDoor(DeltaTime);
+        }
     }
 }
 
 void UOpenDoor::OpenDoor(float DeltaTime) {
-    CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 0.5f);
+    CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
+    FRotator DoorRotation = GetOwner()->GetActorRotation();
+    DoorRotation.Yaw = CurrentYaw;
+    GetOwner()->SetActorRotation(DoorRotation);
+}
+
+void UOpenDoor::CloseDoor(float DeltaTime) {
+    CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
     FRotator DoorRotation = GetOwner()->GetActorRotation();
     DoorRotation.Yaw = CurrentYaw;
     GetOwner()->SetActorRotation(DoorRotation);
